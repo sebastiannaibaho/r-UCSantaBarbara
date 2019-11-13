a0447905f791b43fb1d07378320a79a8af941ff6
edwardD2 <46702102+edwardD2@users.noreply.github.com>
2019-11-12 19:12:51 -0800

    added .csv creation functions, data visuals

--- Post_Scraper.py
+++ Post_Scraper.py
@@ -6,6 +6,10 @@ import urllib.parse
 import urllib.request
 from datetime import datetime
 import praw
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #
 # Using the reddit api gets us comments, but is really slow. I only have it getting the top level comments (which
@@ -20,14 +24,16 @@ import praw
 
 USE_REDDIT_API = False  # true will use the api and also include comments
 PRINT_TO_FILE = True  # false will print info to screen
+CREATE_CSV = True  # have true for writing csv files (using pushshift)
 
 # If you put -1 for num_posts, then the program will run until the FIRST_POST_UTC time which is defaulted to beginning
 # of the subreddit. Not too bad with USE_REDDIT_API set to False
 NUM_POSTS = 1000
 
-OUTPUT_FILE = "output.txt"
+OUTPUT_FILE = "output.csv"  # change to .csv
 OUTPUT_LINE_WIDTH = 120
 FORMAT_TEXT = True  # makes the lines not exceed OUTPUT_LINE_WIDTH
+VISUALS = True  # true if you want to create/display visuals
 
 # The amount of posts received for each request to pushshift, max is 1000
 POSTS_PER_REQUEST = 1000
@@ -68,6 +74,8 @@ def fuck_emojis(fn, str, input):
     try:
         if len(input) == 4:  # posts
             fn(str % (input[0], input[1], input[2], format_text(input[3], OUTPUT_LINE_WIDTH)))
+        elif len(input) == 3:  # .csv writing
+            fn(str % (input[0], format_text_csv(input[1]), input[2]))
         else:  # comments
             fn(str % (input[0], input[1], re.sub('\n', '\n   ', format_text(input[2], OUTPUT_LINE_WIDTH - 3))))
         return 0
@@ -90,7 +98,7 @@ def print_posts_with_api(ps, fn):
     deleted = 0
     submissions = []
 
-    for i in range(0, math.ceil(len(ids)/1000)):
+    for i in range(0, math.ceil(len(ids) / 1000)):
         lower = i * 1000
         upper = lower + 1000 if lower + 1000 < len(ids) else len(ids)
         print('Loading posts %d - %d from api' % (lower + 1, upper))
@@ -119,15 +127,27 @@ def print_posts_with_pushshift(ps, fn):
 
     for p in ps:
         if valid_post(p['selftext']):
-            did_not_print = fuck_emojis(fn, '"%s" by %s (%d):\n%s\n', [p['title'], p['author'], p['score'],
+            if CREATE_CSV:
+                if p['link_flair_richtext']:
+                    did_not_print = fuck_emojis(fn, '"%s";-%s;-%s\n', [p['title'], p['selftext'], p['link_flair_text']])
+                else:
+                    did_not_print = fuck_emojis(fn, '"%s";-%s;-%s\n', [p['title'], p['selftext'], None])
+                deleted += did_not_print
+            else:
+                did_not_print = fuck_emojis(fn, '"%s" by %s (%d):\n%s\n', [p['title'], p['author'], p['score'],
                                                                        p['selftext']])
-            deleted += did_not_print
-            if not did_not_print:
+                deleted += did_not_print
+            if not did_not_print and not CREATE_CSV:
                 fn('--------------------------------------\n')
         else:
             deleted += 1
 
     return deleted
+
+
+def format_text_csv(content):  # formatting for .csv file
+    content = content.replace('\n', '')  # delete existing new lines so everything is on one line
+    return content
 
 
 # The max can only be 1000, posts_per_request should not be used for remainder of code
@@ -136,7 +156,7 @@ size_per_request = POSTS_PER_REQUEST if POSTS_PER_REQUEST < 1000 else 1000
 
 PS_URL = 'https://api.pushshift.io/reddit/search/submission/?subreddit=UCSantaBarbara&sort=desc&sort_type=created_utc'
 FIRST_POST_UTC = '1305484091'  # Utc time of very first post, I just changed sort to "asc" to get this
-                               # you can change this to any other value to get posts up to this point
+# you can change this to any other value to get posts up to this point
 CURRENT_TIME_UTC = math.floor((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds())
 # this is the first post we want to start at (currently set to now)
 last_date = f'&before={CURRENT_TIME_UTC}'
@@ -155,13 +175,20 @@ while (FIRST_POST_UTC not in last_date) and (NUM_POSTS == -1 or count < NUM_POST
 
     last_date = '&before=' + str(posts[-1]['created_utc'])
 
-
 if PRINT_TO_FILE:
-    f = open(OUTPUT_FILE, 'a')
+    f = open(OUTPUT_FILE, 'w')  # okay i changed this to write but i'm not sure if you wanted to keep append maybe we
+                                 # just change it for what we want to use?  -Edward
 if USE_REDDIT_API:
     num_deleted = print_posts_with_api(posts, f.write if PRINT_TO_FILE else print)
 else:
     num_deleted = print_posts_with_pushshift(posts, f.write if PRINT_TO_FILE else print)
+
+if VISUALS:
+    df = pd.read_csv(OUTPUT_FILE, sep=';-', names=['Title', 'Content', 'Category'])
+    #print(df['Title'].size)
+    sns.countplot(x='Category', data=df)
+    plt.show()
+
 if PRINT_TO_FILE:
     f.close()
 
