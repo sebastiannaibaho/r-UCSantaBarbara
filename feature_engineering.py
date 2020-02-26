27bc144ebc0f55e0ec87bd907b0afc35934d2f07
edwardD2 <46702102+edwardD2@users.noreply.github.com>
2020-02-25 20:11:08 -0800

    Add files via upload

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,34 +1,34 @@
 import pickle
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from pathlib import Path
-import collections
-from Data.get_data import get_processed
+from sklearn.feature_selection import chi2
+from nltk.stem import WordNetLemmatizer
+import Data.get_data
+from pprint import pprint
 
-Path("/Pickles").mkdir(parents=True, exist_ok=True)
-
-df = get_processed('UCSantaBarbara_1580367940ExtraFlairs', read=True)[0:10000]
+#df = get_data.get_data(10000, debug=True)
+df = pd.read_json('dataframe.json', dtype=str)
 
 # let's try subsetting the dataframe with only posts with flairs already
-df_sorted = df[df['Category'] != 'None']
-df_sorted = df_sorted[df_sorted['Category'] != 'Image']
+df_sorted = pd.DataFrame({'Title': [], 'Content': [], 'Id': [], 'Category': []})
+i = 0
+for row in range(0, len(df)):
+    if df.iloc[row]['Category'] != 'None':
+        df_sorted.loc[i] = df.iloc[row]
+        i += 1
+
+print(df_sorted.head())
 
 # Label coding
 category_codes = {}
 categories = list(set(df_sorted['Category']))
 [category_codes.update({categories[i]: i}) for i in range(0, len(categories))]
-
-# add category codes
 df_sorted['Category_Code'] = df_sorted['Category']
 df_sorted = df_sorted.replace({'Category_Code': category_codes})
-
-# get rid of categories with less than 3 posts
-deleted = collections.Counter(df_sorted['Category_Code'])
-df_sorted = df_sorted[[deleted[i] >= 3 for i in df_sorted['Category_Code']]]
-
-# convert array to string
-df_sorted['Content'] = [" ".join(i) for i in df_sorted['Content']]
+#print(df_sorted.head())
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(df_sorted['Content'],
@@ -40,8 +40,8 @@ X_train, X_test, y_train, y_test = train_test_split(df_sorted['Content'],
 # Text representation (TF-IDF Vectors)
 # Parameter election
 ngram_range = (1, 2)
-min_df = 10
-max_df = 1.
+min_df = 0.01
+max_df = 0.5
 max_features = 300
 tfidf = TfidfVectorizer(encoding='utf-8',
                         ngram_range=ngram_range,
@@ -55,62 +55,74 @@ tfidf = TfidfVectorizer(encoding='utf-8',
 
 features_train = tfidf.fit_transform(X_train).toarray()
 labels_train = y_train
+#print(features_train.shape)
 
 features_test = tfidf.transform(X_test).toarray()
 labels_test = y_test
-
+#print(features_test.shape)
 from sklearn.feature_selection import chi2
 import numpy as np
 
-for Product, category_id in sorted(category_codes.items()):
-    features_chi2 = chi2(features_train, labels_train == category_id)
-    #print(features_chi2)
-    indices = np.argsort(features_chi2[0])
-    feature_names = np.array(tfidf.get_feature_names())[indices]
-    unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-    bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-    print("# '{}' category:".format(Product))
-    print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-5:])))
-    print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-2:])))
-    print("")
+def get_unigrams(topic):
+    for Product, category_id in sorted(category_codes.items()):
+        if topic == Product:
+            features_chi2 = chi2(features_train, labels_train == category_id)
+            indices = np.argsort(features_chi2[0])
+            sort_chi2 = sorted(features_chi2[0].tolist())
+            feature_names = np.array(tfidf.get_feature_names())[indices]
+            nfeature_names = feature_names.tolist()
+            list_bi = []
+            for v in nfeature_names:
+                if len(v.split(' ')) == 2:
+                    list_bi.append(nfeature_names.index(v))
+            j = 0
+            for i in list_bi:
+                sort_chi2.pop(i - j)
+                j += 1
+            unigram_keys = {}
+            unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+            for k in range(0, len(sort_chi2)):
+                unigram_keys[unigrams[k]] = sort_chi2[k]
+    return unigram_keys
+
 
 # Save files for later
 # X_train
-with open('Models/Pickles/X_train.pickle', 'wb+') as output:
+with open('Pickles/X_train.pickle', 'wb+') as output:
     pickle.dump(X_train, output)
 
 # X_test
-with open('Models/Pickles/X_test.pickle', 'wb+') as output:
+with open('Pickles/X_test.pickle', 'wb+') as output:
     pickle.dump(X_test, output)
 
 # y_train
-with open('Models/Pickles/y_train.pickle', 'wb+') as output:
+with open('Pickles/y_train.pickle', 'wb+') as output:
     pickle.dump(y_train, output)
 
 # y_test
-with open('Models/Pickles/y_test.pickle', 'wb+') as output:
+with open('Pickles/y_test.pickle', 'wb+') as output:
     pickle.dump(y_test, output)
 
 # df
-with open('Models/Pickles/df.pickle', 'wb+') as output:
+with open('Pickles/df.pickle', 'wb+') as output:
     pickle.dump(df, output)
 
 # features_train
-with open('Models/Pickles/features_train.pickle', 'wb+') as output:
+with open('Pickles/features_train.pickle', 'wb+') as output:
     pickle.dump(features_train, output)
 
 # labels_train
-with open('Models/Pickles/labels_train.pickle', 'wb+') as output:
+with open('Pickles/labels_train.pickle', 'wb+') as output:
     pickle.dump(labels_train, output)
 
 # features_test
-with open('Models/Pickles/features_test.pickle', 'wb+') as output:
+with open('Pickles/features_test.pickle', 'wb+') as output:
     pickle.dump(features_test, output)
 
 # labels_test
-with open('Models/Pickles/labels_test.pickle', 'wb+') as output:
+with open('Pickles/labels_test.pickle', 'wb+') as output:
     pickle.dump(labels_test, output)
 
 # TF-IDF object
-with open('Models/Pickles/tfidf.pickle', 'wb+') as output:
+with open('Pickles/tfidf.pickle', 'wb+') as output:
     pickle.dump(tfidf, output)
